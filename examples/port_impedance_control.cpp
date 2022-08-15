d32d22fe7d0fd70125c54200a458a9605eaa5d47
Daniel Larby <dan_larby@hotmail.co.uk>
2022-08-15 17:23:41 +0100

    Fix computing position

--- examples/port_impedance_control.cpp
+++ examples/port_impedance_control.cpp
@@ -49,7 +49,7 @@ int main(int argc, char** argv) {
 
     // equilibrium point is the initial position
     Eigen::Affine3d initial_transform(Eigen::Matrix4d::Map(initial_state.O_T_EE.data()));
-    Eigen::Vector3d position_d(initial_transform.translation() + initial_transform * ee_offset);
+    Eigen::Vector3d position_d(initial_transform * ee_offset);
 
     // set collision behavior
     robot.setCollisionBehavior({{10.0, 10.0, 10.0, 10.0, 10.0, 10.0, 10.0}},
@@ -78,7 +78,7 @@ int main(int argc, char** argv) {
       auto jacobian = translationalJacobian - skew(rotation_transform * ee_offset) * rotationalJacobian;
       Eigen::Map<const Eigen::Matrix<double, 7, 1>> q(robot_state.q.data());
       Eigen::Map<const Eigen::Matrix<double, 7, 1>> dq(robot_state.dq.data());
-      Eigen::Vector3d position(current_transform.translation() + current_transform * ee_offset);
+      Eigen::Vector3d position(current_transform * ee_offset);
 
       // compute error to desired equilibrium pose
       // position error
